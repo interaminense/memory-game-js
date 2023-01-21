df852b5203ff8ec47500b64724ef3c90c112b9a1
Adriano Interaminense <adriano.interaminense@gmail.com>
2023-01-21 17:38:45 -0300

    Avoid inspect react devtools and disable source map for production mode

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { disableReactDevTools } from "./utils";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- src/utils.ts
+++ src/utils.ts
@@ -132,3 +132,25 @@ export function getCards(totalCards: number) {
     flipped: false,
   }));
 }
+declare global {
+  interface Window {
+    __REACT_DEVTOOLS_GLOBAL_HOOK__: any;
+  }
+}
+
+export function disableReactDevTools() {
+  if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== "object") {
+    return;
+  }
+
+  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    if (prop === "renderers") {
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = new Map();
+    } else {
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] =
+        typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] === "function"
+          ? () => {}
+          : null;
+    }
+  }
+}
